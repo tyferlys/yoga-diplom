8c26a13fdd88d0fb19a2d1174a0511bd37148d80
denis klimov <tyferly2003@gmail.com>
2024-10-07 12:25:46 +0400

    yoga-diplom init

--- src/api/poses/schemas.py
+++ src/api/poses/schemas.py
@@ -21,13 +21,16 @@ class PoseOutPagination(BaseModel):
 class PoseFullOut(BaseModel):
     id: int
     source_title: str
+    image: str
+    description: str
+
 
     @classmethod
     def from_db(cls, pose_db) -> "PoseFullOut":
-        return PoseFullOut(id=pose_db[0], source_title=pose_db[1])
+        return PoseFullOut(id=pose_db[0], source_title=pose_db[1], image=pose_db[2], description=pose_db[3])
 
     @classmethod
     def from_db_list(cls, poses_db) -> list["PoseFullOut"]:
         return [
-            PoseFullOut(id=item[0], source_title=item[1]) for item in poses_db
+            PoseFullOut(id=item[0], source_title=item[1], image=item[2], description=item[3]) for item in poses_db
         ]
